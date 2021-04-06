3618a08c26adea8d1e20093f8616229e88221087
Guido Aspauzo Baez <39416974+gnabcel@users.noreply.github.com>
2021-04-06 22:25:57 +1200

    Update FooBar_leve2.2_Maze.py

--- FooBar_leve2.2_Maze.py
+++ FooBar_leve2.2_Maze.py
@@ -1,32 +1,37 @@
 ## prepare-the-bunnies-escape
 
 def solution(maze):
-    DestX = len(maze[0])
-    DestY = len(maze)
+    steps = 999999
+    for count, value in enumerate([j for i in maze for j in i]): ## wall tracker
+        if value == 1:
+            maze[count // len(maze)][count % len(maze)] = 0
+            if counter(maze) != 0 and counter(maze) < steps:
+                steps = counter(maze)
+            maze[count // len(maze)][count % len(maze)] = 1
+    return steps
+
+def counter(maze):
+    DestX = len(maze)
     x = 0
     y = 0
     steps = [0]
-
-    while (x,y) is not (DestX - 1, DestY - 1):
-        print(steps)
-        if maze[x][y + 1] == 0 and y + 1 < DestY: # check the Right
-            y += 1
-            steps.append(DestX*x + y)
-            continue
-        if maze[x + 1][y] == 0 and x + 1 < DestX: # check the down
+    while DestX * DestX - 1 not in steps :
+        if x + 1 < DestX and maze[x + 1][y] == 0 and DestX*(x+1) + y not in steps: # check the down
             x += 1
             steps.append(DestX*x + y)
             continue
-        # if maze[x - 1][y] == 0 and : # check the down
-        #     x += 1
-        #     steps.append(DestX*x + y)
-        #     continue
-        print(x,y)
-        print(steps.count())
-    return steps.count()
+        if y + 1 < DestX and maze[x][y + 1] == 0 and DestX*x + y + 1 not in steps: # check the Right
+            y += 1
+            steps.append(DestX*x + y)
+            continue
+        if y - 1 >= 0 == maze[x][y - 1] and DestX*x + y - 1 not in steps: # check left
+            y -= 1
+            steps.append(DestX*x + y)
+            continue
+        steps = []
+        break
+    return len(steps)
 
-asd = [1,2,3]
-print(asd.count(element))
-print(solution([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]))
-
+# Test line:
+# print(solution([[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]))
 
